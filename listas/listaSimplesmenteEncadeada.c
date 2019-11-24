96940309fab8988bf613b4922d62167394ddd213
adeonita <adeonita.sousa@gmail.com>
2019-11-24 19:34:12 -0300

    Implementaçao de uma inserçao ordenada

--- listas/listaSimplesmenteEncadeada.c
+++ listas/listaSimplesmenteEncadeada.c
@@ -26,15 +26,38 @@ Nodo *cria_nodo(){
 }
 
 /*Insere no inicio da lista
+//Pode ser utilizado para implementar a pilha, pois a pilha só trabalha com o topo
 //Como parâmetros são passados um ponteiro que representa a lista (inicio) e um valor (n)
-*/
+
 void insere(Lista *inicio_da_lista, int n){
     Nodo *novo = cria_nodo();  //Aloca um novo espaco
     novo->info = n;  //Atribui ao campo info o valor n
     novo->prox = inicio_da_lista->primeiro_no; //Novo->prox recebe o inicio da lista pois o enlaçe é feito pelo ponteiro
     inicio_da_lista->primeiro_no = novo;  //E o inicio da lista recebe o novo
 }
+*/
 
+void insere_ordenado(Lista *inicio_da_lista, int n){
+    Nodo *anterior = NULL;  //Ponteiro para o elemento anterior
+    Nodo *atual = inicio_da_lista->primeiro_no;  //Ponteiro auxiliar (*atual) recebe o primeiro nó do inicio da lista
+
+    while(atual != NULL && atual->info < n){  //Varre a lista procurando onde inserir o elemento
+        anterior =  atual;
+        atual = atual -> prox;
+    }
+
+    Nodo *novo = cria_nodo();  //Cria o novo elemento
+    novo->info = n;
+
+    if(anterior == NULL){ //Estou andando com atual, e se anterior que é a lista vazia for igual a nulll, insiro no inicio
+        novo->prox = inicio_da_lista->primeiro_no;
+        inicio_da_lista->primeiro_no = novo;
+    }else{
+        novo->prox = anterior->prox;
+        anterior->prox = novo;
+    }
+
+} 
 
 void exibe_lista(Lista *inicio){
 
@@ -50,15 +73,14 @@ void exibe_lista(Lista *inicio){
    //Exibição da lista como for
     for(atual = inicio->primeiro_no; atual != NULL; atual = atual->prox){
         printf("%d, ", atual->info);
-    }
-    
+    }    
 }
 
 int main(){
     Lista *lista = cria_lista();
     int opcao = ' ';
 
-    if(cria_lista){
+    if(lista){
         while(opcao != 0){
             printf("Digite 1 para inserir 0 para sair, 2 para exibir: ");
             scanf("%d",&opcao);
@@ -66,7 +88,7 @@ int main(){
                 int n;
                 printf("Digite o numero a ser inserido: ");
                 scanf("%d", &n);
-                insere(lista, n);
+                insere_ordenado(lista, n);
                 printf("\nO numero %d foi inserido na lista\n", n);
             }if(opcao == 2){
                 printf("Os elementos dessa lista são...");
