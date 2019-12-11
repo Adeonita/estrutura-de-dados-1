c686bcc20b762a5ea553e97022db4de0120da690
adeonita <adeonita.sousa@gmail.com>
2019-12-11 16:07:18 -0300

    Manutenção da função inserir

--- provaEd/fila.c
+++ provaEd/fila.c
@@ -3,8 +3,7 @@
 
 typedef struct  nodo{
    int info;
-   struct Nodo *prox;  //Ponteiro para uma estrutura igual a ela (Estrutura autoreferenciada)
-   
+   struct nodo *prox;  //Ponteiro para uma estrutura igual a ela (Estrutura autoreferenciada)
 }Nodo;
 
 typedef struct fila{
@@ -24,35 +23,41 @@ Fila *cria_fila(){
     return inicio_da_fila;
 }
 
-int isEmpty(Fila *incio_da_fila){
-    return incio_da_fila->inicio == NULL;
-}
 
 void insert(Fila *inicio_da_fila, int n){
     Nodo *novo = cria_nodo();
     novo->info = n;  //inicialização do novo nó
     novo->prox = NULL; //inicialização do novo nó
-    if(isEmpty){
-        inicio_da_fila->inicio = novo;
-        inicio_da_fila->fim = novo;
+    if(inicio_da_fila->fim != NULL){
+        inicio_da_fila->fim->prox = novo;
     }else{
-        inicio_da_fila->fim = novo;
+        inicio_da_fila->inicio = novo;
+    }inicio_da_fila->fim = novo;
+}
+
+void exibeFila(Fila *incio_da_fila){
+    printf("\n\nListando a fila...\n\n");
+    for(Nodo *i = incio_da_fila->inicio; i != NULL; i = i->prox){
+        printf("%d -> ", i->info);
     }
 }
 
-void exibeFila(Fila *incio_da_fila)
+
 
 int main(){
     int opcao = ' ';
     Fila *fila = cria_fila();
 
     while(opcao =! 0){
-        printf("Digite 1 para inserir, 2 para retirar, 3 para listar e 0 para sair");
-        if(opcao = 1){
+        printf("Digite 1 para inserir, 2 para retirar, 3 para listar e 0 para sair\n");
+        scanf("%d",&opcao);
+        if(opcao == 1){
             int n;
             printf("Digite um numero para inserir: ");
             scanf("%d", &n);
             insert(fila, n);
+        }if(opcao == 3){
+            exibeFila(fila);
         }
     }
     
