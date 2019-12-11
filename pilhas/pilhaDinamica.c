64e1b84ac28c4b4cf20d0a321e5e09131dd2f6aa
adeonita <adeonita.sousa@gmail.com>
2019-12-11 11:06:07 -0300

    Implementação de uma pilha dinâmica

---
+++ pilhas/pilhaDinamica.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct nodo{
+    int info;
+    struct nodo *prox; //Ponteiro para próxima posição do tipo nodo
+}Nodo;
+
+/*Retorna um ponteiro para o início da pilha*/
+Nodo *cria_nodo(){
+    Nodo *novo = (Nodo*) malloc(sizeof(Nodo));
+    return novo;
+}
+
+Nodo *inserir_no_inicio(Nodo *inicio, int n){
+    Nodo *novo = cria_nodo();
+    novo->info = n;
+
+    if(inicio == NULL){
+        inicio = novo;
+        novo->prox = NULL;
+   }else{
+        novo->prox = inicio;
+        inicio = novo;
+   }
+    return inicio;
+}
+
+Nodo *inserir_no_fim(Nodo *inicio, int n){
+    
+    Nodo *novo = cria_nodo();
+    novo->info = n;
+    
+    
+    if(inicio == NULL){
+        novo->prox = NULL;
+        inicio = novo;
+    }else{
+        Nodo *atual;
+        atual = inicio;
+        while(atual->prox != NULL){
+            atual = atual->prox;
+        }
+        novo->prox = NULL;
+        atual->prox = novo;
+    }
+
+}
+
+void *exibe_lista(Nodo *inicio){
+
+    Nodo *atual = inicio;
+
+    while(atual != NULL){
+        printf("%d, ", atual->info);
+        atual = atual->prox;
+    }
+}
+
+
+
+int main(){
+    Nodo *inicio = NULL; //Cria uma lista vazia
+    int opcao = ' ';
+    int n;
+
+    while (opcao != 0 ){
+        printf("Insira 1:Inserir 0:sair ");
+        scanf("%d", &opcao);
+        if(opcao == 1){
+            printf("Digite o numero a ser inserido: ");
+            scanf("%d", &n);
+            inicio = inserir_no_inicio(inicio, n);
+        }
+    }
+ 
+    exibe_lista(inicio);
+
+}
